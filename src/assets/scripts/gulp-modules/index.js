6d60026a5b8ff59f0e38f04f96aa66b35960b72b
AndriiSolodaiev <andrii.solodaiev@gmail.com>
2024-09-11 22:35:16 +0300

    mobile styles done

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -1,23 +1,24 @@
-import Swiper, { Autoplay, EffectFade } from 'swiper';
+import Swiper, { Autoplay, EffectFade, Pagination } from 'swiper';
 import { Navigation } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
+import 'swiper/css/pagination';
 import { gsap, ScrollTrigger, CustomEase } from 'gsap/all';
 
 import '../modules/helpers/imgParallax';
 
 import { initSmoothScrolling } from '../modules/scroll/leniscroll';
-
+import device from 'current-device';
 initSmoothScrolling();
 gsap.registerPlugin(ScrollTrigger, CustomEase);
 
-const expandArrow = document.querySelector(".expand-arrow")
-const partnersList = document.querySelector(".partners-list")
+const expandArrow = document.querySelector('.expand-arrow');
+const partnersList = document.querySelector('.partners-list');
 if (expandArrow) {
-  expandArrow.addEventListener("click", function () {
-    partnersList.classList.toggle("open-list")
-    expandArrow.classList.toggle("open-list")
-  })
+  expandArrow.addEventListener('click', function() {
+    partnersList.classList.toggle('open-list');
+    expandArrow.classList.toggle('open-list');
+  });
 }
 
 const fillerTl = gsap.timeline({
@@ -25,31 +26,166 @@ const fillerTl = gsap.timeline({
     trigger: '.filler-photo1',
     start: 'top bottom', // when the top of the trigger hits the top of the viewport
     end: 'bottom center', // end after scrolling 500px beyond the start
-     // smooth scrubbing, takes 1 second to "catch up" to the
+    // smooth scrubbing, takes 1 second to "catch up" to the
     // markers: true,
   },
 });
 fillerTl
-.to('.filler-photo1 .right-border', 
-  {
-    xPercent:100,
+  .to('.filler-photo1 .right-border', {
+    xPercent: 100,
     duration: 1.7,
     delay: 0.3,
-  }
-)
-.to('.filler-photo1 .left-border', 
-  {
-    xPercent:-100,
-    duration: 1.7,
-  },
-  '<'
+  })
+  .to(
+    '.filler-photo1 .left-border',
+    {
+      xPercent: -100,
+      duration: 1.7,
+    },
+    '<',
+  )
+  .from(
+    '.filler-photo1 img',
+    {
+      yPercent: 20,
+      duration: 1.5,
+    },
+    '<',
+  );
 
-)
-.from('.filler-photo1 img', 
-  {
-    yPercent: 20,
-    duration: 1.5,
+const swiperStates = new Swiper('.swiper-state', {
+  modules: [Autoplay],
+  speed: 5000,
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  // slidesPerView: 1,
+  autoplay: { delay: 10 },
+  breakpoints: {
+    // 360: {
+    //   slidesPerView: 1.1,
+    //   spaceBetween: 8,
+    // },
+    // 768: {
+    //   slidesPerView: 2,
+    //   //   spaceBetween: 20,
+    // },
+    // 1366: {
+    //   // spaceBetween: 20,
+    //   slidesPerView: 3,
+    // },
   },
-  '<'
+});
+const swiperStories = new Swiper('.swiper-stories', {
+  modules: [Pagination],
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  speed: 1000,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  on: {
+    init: function() {
+      const firstVideo = document.querySelector('.swiper-slide video');
+      firstVideo.play();
+      firstVideo.style.filter = 'none';
+      document
+        .querySelector('.swiper-slide video')
+        .addEventListener('ended', function videoEnded() {
+          swiperStories.slideTo(0);
+          firstVideo.style.filter = 'grayscale(1)';
+          firstVideo.removeEventListener('ended', videoEnded);
+        });
+    },
+    beforeTransitionStart: function(swiper) {
+      let activeSlideVideo = swiper.slides[swiper.realIndex].querySelector('video');
 
-)
+      if (activeSlideVideo) {
+        activeSlideVideo.addEventListener('ended', function videoEnded() {
+          swiper.slideTo(swiper.slides.length === swiper.realIndex + 1 ? 0 : swiper.realIndex + 1);
+          activeSlideVideo.removeEventListener('ended', videoEnded);
+        });
+        activeSlideVideo.currentTime = 0;
+        document.querySelectorAll('.swiper-slide video').forEach(function(video) {
+          video.style.filter = 'grayscale(1)';
+          if (!video.paused) {
+            video.pause();
+          }
+        });
+
+        activeSlideVideo.play();
+        activeSlideVideo.style.filter = 'none';
+      }
+    },
+  },
+});
+
+const questionsTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.questions',
+    start: 'top+=20% center', // when the top of the trigger hits the top of the viewport
+    end: 'bottom center', // end after scrolling 500px beyond the start
+    // smooth scrubbing, takes 1 second to "catch up" to the
+    // markers: true,
+  },
+});
+questionsTl.from('.question', {
+  opacity: 0,
+  yPercent: -300,
+  stagger: {
+    each: 0.1, // Час між анімаціями кожного елемента
+    from: 'end', // Анімація починається з останнього елемента
+  },
+  duration: 1,
+  ease: 'bounce.out',
+});
+gsap.to('.about', {
+  scrollTrigger: {
+    trigger: '.about', // Елемент, який триггерить анімацію
+    start: 'top bottom', // Коли верх другого блоку досягне низу вікна
+    end: 'top top', // Коли верх другого блоку досягне верху вікна
+    scrub: true, // Анімація синхронізується зі скролом
+    pin: '.hero', // Фіксуємо перший блок на місці
+    pinSpacing: false, // Видаляємо простір, коли блокує перший блок
+  },
+  // y: '-100%', // Другий блок наїжджає на перший
+});
+gsap.to('.question__title', {
+  scrollTrigger: {
+    trigger: '.courses ', // Елемент, який триггерить анімацію
+    start: 'top bottom', // Коли верх другого блоку досягне низу вікна
+    end: 'top center', // Коли верх другого блоку досягне верху вікна
+    scrub: true, // Анімація синхронізується зі скролом
+    pin: '.question__title', // Фіксуємо перший блок на місці
+    pinSpacing: false, // Видаляємо простір, коли блокує перший блок
+  },
+  opacity: 0,
+  scale: 1.2,
+  // y: '-100%', // Другий блок наїжджає на перший
+});
+
+gsap.to('.filler-photo-2', {
+  scrollTrigger: {
+    trigger: '.reviews', // Елемент, який триггерить анімацію
+    start: () => window.innerWidth * -0.45 + ' top', // Коли верх другого блоку досягне низу вікна
+    end: 'top top', // Коли верх другого блоку досягне верху вікна
+    scrub: true, // Анімація синхронізується зі скролом
+    pin: '.filler-photo-2', // Фіксуємо перший блок на місці
+    pinSpacing: false, // Видаляємо простір, коли блокує перший блок
+  },
+  scale: 1.1,
+});
+const swiperReviews = new Swiper('.swiper-reviews', {
+  modules: [Autoplay],
+  speed: 5000,
+  slidesPerView: 'auto',
+  spaceBetween: 8,
+
+  autoplay: { delay: 10 },
+});
+const swiperYoutube = new Swiper('.swiper-youtube', {
+  modules: [Autoplay],
+  speed: 5000,
+  slidesPerView: 'auto',
+  autoplay: { delay: 10 },
+});
